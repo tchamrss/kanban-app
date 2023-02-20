91de546aab3004a674c88e28486ca020855b12e4
Russell <russell.tchamba@gmail.com>
2023-02-20 22:30:46 +0100

    removed password field

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -4,25 +4,27 @@ from rest_framework import serializers
 from .models import Tasks
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    password = serializers.CharField(min_length=5, write_only=True)
+    #password = serializers.CharField(min_length=5, write_only=True)
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'username','email', 'password']
+        fields = ['id', 'first_name', 'last_name', 'username','email']
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+        required = False  # Make the author field optional
         
-    def create(self, validated_data):
+    """  def create(self, validated_data):
         password = validated_data.pop('password', None)
         instance = self.Meta.model(**validated_data)
         if password is not None:
             instance.set_password(password)
         instance.save()
-        return instance
+        return instance """
     
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
-    author = UserSerializer()
+    author = UserSerializer(required=False)
     class Meta:
         model = Tasks
         fields = ['title', 'description', 'created_at','author','priority','due_date']
-        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}} #extra_kwargs allows us to configure few extra settings in our Serializer,here making sure password is write only and more than 5 characters.
+       #extra_kwargs = {'password': {'write_only': True, 'min_length': 5}} #extra_kwargs allows us to configure few extra settings in our Serializer,here making sure password is write only and more than 5 characters.
 
         
